3ecb650ebf8832c3ad6e204221b5b07d4cd75d42
Mohammd Mujaheed <mujaheedsami13@gmail.com>
2023-03-14 19:07:50 +0530

    Reverse Array Part II

---
+++ Problems/DSA101/E1/reverse-array-p2.js
@@ -0,0 +1,16 @@
+// => https://oj.masaischool.com/contest/5369/problem/402
+
+let arr = [6, 7, 8, 9, 10];
+let n = arr.length;
+let k = 3;
+
+let str = '';
+for(let j=0; j<=k-1; j++){
+    str+=arr[j]+' ';
+}
+
+for(let i=n-1; i>=k; i--){
+    str+=arr[i]+' ';
+}
+
+console.log(str);
