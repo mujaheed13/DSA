3e54495ed91d80f0a57ce5e99a8e5a91006737c3
Mohammd Mujaheed <mujaheedsami13@gmail.com>
2023-03-14 18:23:52 +0530

    Two & Three

---
+++ Problems/DSA101/E1/two-and-three.js
@@ -0,0 +1,14 @@
+// => https://oj.masaischool.com/contest/5369/problem/101
+let num = 6;
+
+for(let i=1; i<=num; i++){
+    console.log(
+        i%2==0 && i%3==0
+            ? "Both"
+        : i%2==0 
+            ? "Two"
+        : i%3==0 
+            ? "Three"
+        : "None"
+    );
+}
