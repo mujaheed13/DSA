c9b01dc2b09148e915eb20c4c2589398e1fe3a21
Mohammd Mujaheed <mujaheedsami13@gmail.com>
2023-03-16 19:46:39 +0530

    Sachin Fan

---
+++ Problems/DSA101/E2/sachin-fan.js
@@ -0,0 +1,24 @@
+// ==> https://oj.masaischool.com/contest/5480/problem/202
+
+function fun(arr, n){
+
+    let ans = 0;
+
+    for(let i=0; i<n; i++){
+        let sub_arr = [];
+        for(let j=i; j<n; j++){
+            sub_arr.push(arr[j])
+            if(sub_arr.includes(10)){
+                ans++;
+            }
+        }
+        
+    }
+
+    console.log(ans);
+}
+
+
+let arr = [1, 2, 10];
+let n = arr.length;
+fun(arr, n);
