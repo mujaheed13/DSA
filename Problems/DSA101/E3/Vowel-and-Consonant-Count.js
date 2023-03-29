dcd61c0ac6686da3592f991c0c5c7e26f0d8fde1
Mohammd Mujaheed <mujaheedsami13@gmail.com>
2023-03-29 17:09:59 +0530

    Vowel & Consonant Count

---
+++ Problems/DSA101/E3/Vowel-and-Consonant-Count.js
@@ -0,0 +1,28 @@
+// https://oj.masaischool.com/contest/5568/problem/101
+
+function fun(str, n){
+    let vowel = "aeiou";
+    let consonent = "bcdfghjklmnpqrstwxyz";
+    
+    let v = 0;
+    let c = 0;
+
+    for(let i=0; i<n; i++){
+        for(let j=0; j<vowel.length; j++){
+            if(str[i]===vowel[j]){
+                v++;
+            }
+        }
+        for(let j=0; j<consonent.length; j++){
+            if(str[i]===consonent[j]){
+                c++;
+            }
+        }
+    }
+
+    console.log(v, c);
+}
+
+let str = "mujaheed";
+let n = str.length;
+fun(str, n);
