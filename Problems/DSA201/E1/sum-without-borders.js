2e27600ac6cc5efb299f298ed7d129448b2ef10d
Mohammd Mujaheed <mujaheedsami13@gmail.com>
2023-04-05 14:51:18 +0530

    Sum without borders

---
+++ Problems/DSA201/E1/sum-without-borders.js
@@ -0,0 +1,22 @@
+function sumWithoutBorders(N, M, matrix) {
+  let ans = 0;
+  for (let i = 1; i < N - 1; i++) {
+    for (let j = 1; j < M - 1; j++) {
+      ans += matrix[i][j];
+    }
+  }
+
+  console.log(ans);
+}
+
+let matrix = [
+  [1, 3, 4, 3],
+  [7, 5, 7, 5],
+  [1, 1, 7, 2],
+  [11, 33, 6, 8],
+];
+
+let n = matrix.length;
+let m = matrix[0].length;
+
+sumWithoutBorders(n, m, matrix);
