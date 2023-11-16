902cbcf30d63a7021cac00ce75f3f890d430aff6
mujaheed13 <mujaheedsami13@gmail.com>
2023-11-16 22:40:34 +0530

    Solved: Ladder-1 Problem1

---
+++ Recap/Ladder-1/L1P1.js
@@ -0,0 +1,15 @@
+// ? https://oj.masaischool.com/contest/402/problem/1
+
+function masaiSchoolHurray(n) {
+  console.log(
+    n % 35 == 0
+      ? "Masai School"
+      : n % 7 == 0
+      ? "Masai"
+      : n % 5 == 0
+      ? "School"
+      : "Hurray!"
+  );
+}
+let n = 7; // ! Masai
+masaiSchoolHurray(n);
