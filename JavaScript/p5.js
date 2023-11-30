de0dc79f607e31433f91ac3157031be59744780c
Mohammd Mujaheed <mujaheedsami13@gmail.com>
2023-11-30 22:52:26 +0530

    JavaScript: P5

---
+++ JavaScript/p5.js
@@ -0,0 +1,6 @@
+// ? https://leetcode.com/problems/apply-transform-over-each-element-in-array/?envType=study-plan-v2&envId=30-days-of-javascript
+
+var map = function (arr, fn) {
+  const ans = arr.map(fn);
+  return ans;
+};
