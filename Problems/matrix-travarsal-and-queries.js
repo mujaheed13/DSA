0cab870b42106a2ecdb6cef241c2dc63de34e854
mujaheed13 <mujaheedsami13@gmail.com>
2024-02-01 17:48:04 +0530

    test

---
+++ Problems/matrix-travarsal-and-queries.js
@@ -0,0 +1,6 @@
+// You are given a matrix of size n x m , and two types of queries are to be performed on this matrix. The two types of queries are
+
+// q = 1, for this type of query, the matrix is to be traversed, as shown in Fig. 1
+
+// q = 2, for this type of query, the matrix is to be traversed as shown in Fig. 2
+
