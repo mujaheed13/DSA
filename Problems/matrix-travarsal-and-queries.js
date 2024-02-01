6c88e6c665d4e80d7acf8c34222d8a03ee4de04e
mujaheed13 <mujaheedsami13@gmail.com>
2024-02-01 18:02:43 +0530

    Matrix travarsal & Queries

--- Problems/matrix-travarsal-and-queries.js
+++ Problems/matrix-travarsal-and-queries.js
@@ -4,3 +4,30 @@
 
 // q = 2, for this type of query, the matrix is to be traversed as shown in Fig. 2
 
+let ans = "";
+if(q==1){
+    for(let i=0; i<N; i++){
+        if(i%2==0){
+            for(let j=0; j<N; j++){
+             ans+=arr[i][j]+=" ";
+            }
+        } else {
+            for(let j=N-1; j>=0; j--){
+                ans+=arr[i][j]+=" ";
+            }
+        }
+    }
+} else {
+    for(let i=0; i<N; i++){
+        if(i%2==0){
+            for(let j=N-1; j>=0; j--){
+             ans+=arr[i][j]+=" ";
+            }
+        } else {
+            for(let j=0; j<N; j++){
+                ans+=arr[i][j]+=" ";
+            }
+        }
+    }
+}
+console.log(ans);
